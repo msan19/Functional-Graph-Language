64d80c6f349bb6dd39e9d7791fcbad1764630900
Simonbolsen <Simonbolsen1234@gmail.com>
2021-03-12 13:33:09 +0100

    functionNode complete

---
+++ src/ASTLib/Nodes/TypeNodes/FunctionTypeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASTLib.Nodes.TypeNodes
+{   
+    public class FunctionTypeNode: TypeNode
+    {
+        public TypeNode ReturnType { get; private set; }
+        public List<TypeNode> ParameterTypes { get; private set; }
+
+        public FunctionTypeNode(TypeNode returnType, List<TypeNode> parameterTypes) : base(Type.Function)
+        {
+            ReturnType = returnType;
+            ParameterTypes = parameterTypes;
+        }
+    }
+}
---
+++ src/ASTLib/Nodes/TypeNodes/TypeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASTLib.Nodes.TypeNodes
+{
+    public enum Type
+    {
+        Real,
+        Integer,
+        Function
+    }
+    
+    public class TypeNode: Node
+    {
+        public Type Type { get; private set; }
+
+        public TypeNode(Type type)
+        {
+            Type = type;
+        }
+    }
+}
