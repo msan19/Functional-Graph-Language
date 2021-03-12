6b59fe59a8d1357f591484d35eee291c5dbb4448
xomnez <magnus3andersen@gmail.com>
2021-03-12 13:32:38 +0100

    wip

--- src/LexParserLib/ASTBuilder.cs
+++ src/LexParserLib/ASTBuilder.cs
@@ -9,10 +9,48 @@ namespace LexParserLib
     {
         public AST GetAST(ASTNode root)
         {
-
+            // root.Children
+            // foreach (var childNode in root.Children)
+            // {
+            //     ASTNode test = childNode;
+            //     if (true)
+            //     {
+            //         
+            //     }
+            // }
+            
             List<FunctionNode> functionNodes = new List<FunctionNode>();
             List<ExportNode> exportNodes = new List<ExportNode>();
             return new AST(functionNodes, exportNodes);
         }
+
+        public AST VisitDeclarations(ASTNode node)
+        {
+            if (node.Children.Count == 1)
+            {
+                List<FunctionNode> functionNodes = new List<FunctionNode>();
+                List<ExportNode> exportNodes = new List<ExportNode>();
+                AST ast = new AST(functionNodes, exportNodes);
+
+                if (node.Children[0].Symbol.Name == "export")
+                {
+                    // Create export node
+                }
+                else
+                {
+                    // Create function node
+                }
+            }
+            return null;
+        } 
+        
+        public Node VisitDeclaration(ASTNode node)
+        {
+            return null;
+        } 
+        
+        
+        
     }
+    
 }
