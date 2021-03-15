b6985a39efe71c1ef2e5e4674d30ff83483aa5dd
esb179 <eniel19@student.aau.dk>
2021-03-15 11:31:53 +0100

    noget af interpretorhelper

--- src/InterpreterLib/InterpretorHelper.cs
+++ src/InterpreterLib/InterpretorHelper.cs
@@ -19,6 +19,66 @@ namespace InterpreterLib
             throw new NotImplementedException();
         }
 
+        public int SubtractionInteger(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SubtractionReal(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MultiplicationInteger(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double MultiplicationReal(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DivisionInteger(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double DivisionReal(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ModuloInteger(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ModuloReal(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AbsoluteInteger(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double AbsoluteReal(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PowerInteger(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double PowerReal(ExpressionNode node)
+        {
+            throw new NotImplementedException();
+        }
+
         public int IdentifierFunction(ExpressionNode node)
         {
             throw new NotImplementedException();
