0e07b855048bca5b6dd5a1a5a5d8eba3dc4dca3c
Theodor Risager <theodor349@gmail.com>
2021-03-15 12:55:10 +0100

    Moved Input to lexer to input param

--- Compiler/src/Main/Program.cs
+++ Compiler/src/Main/Program.cs
@@ -23,7 +23,11 @@ namespace Main
             Interpreter interpreter = new Interpreter(new InterpretorHelper());
             FileGenerator fileGenerator = new FileGenerator(new FileHelper());
 
-            AST ast = lexParse.Run();
+            string input = "export 5.5 + 33.3 export 5.5 + 33.3 " +
+                                   "func: (integer) -> integer " +
+                                   "func(p) = |z| * y + x / i ^ (17 - 0.1 mod 2)";
+
+            AST ast = lexParse.Run(input);
             referenceHandler.InsertReferences(ast);
             typeChecker.CheckTypes(ast);
             var output = interpreter.Interpret(ast);
