59b0952b7e5cd3607ad3b6b8b816ebf51554a840
msan19 <msan19@student.aau.dk>
2021-03-18 14:06:40 +0100

    First test for TypeChecker.CheckTypes

--- Compiler/Tests/TypeCheckerLib.Tests/TypeCheckerTests.cs
+++ Compiler/Tests/TypeCheckerLib.Tests/TypeCheckerTests.cs
@@ -1,17 +1,53 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using ASTLib;
+using ASTLib.Nodes;
+using ASTLib.Nodes.ExpressionNodes;
+using NSubstitute;
 
 namespace TypeCheckerLib.Tests
 {
     [TestClass]
     public class TypeCheckerTests
     {
+        
+        # region CheckTypes
         [TestMethod]
-        public void Test()
+        public void CheckTypes_TwoExportNodesAndNoFunctionNodes_CalledCorrectNumberOfTimes()
         {
+            int actualNumVisitExportCalls = 0;
+            int expectedNumVisitExportCalls = 2;
+            
+            // Mock type helper
+            ITypeHelper typeHelper = Substitute.For<ITypeHelper>();
+            typeHelper.VisitExport(Arg.Do<ExportNode>(exp => actualNumVisitExportCalls++));
 
+            ITypeChecker typeChecker = new TypeChecker(typeHelper);
+            
+            // Create AST with TwoExportNodesAndNoFunctionNodes
+            FunctionCallExpression funcCallExpr = new FunctionCallExpression("f", new List<ExpressionNode>(), 1, 1);
+            List<ExportNode> exportNodes = new List<ExportNode>()
+            {
+                new ExportNode(funcCallExpr, 1, 1),
+                new ExportNode(funcCallExpr, 2, 2)
+            };
+            AST ast = new AST(new List<FunctionNode>(), exportNodes, 1, 1);
+
+            typeChecker.CheckTypes(ast);
+
+            Assert.AreEqual(expectedNumVisitExportCalls, actualNumVisitExportCalls);
         }
+        
+        [TestMethod]
+        public void CheckTypes_NoExportNodesAndFourFunctionNodes_CalledCorrectNumberOfTimes()
+        {
+            
+        }
+        
+        # endregion
+        
+        # region Dispatch
+        # endregion
+
     }
 }
