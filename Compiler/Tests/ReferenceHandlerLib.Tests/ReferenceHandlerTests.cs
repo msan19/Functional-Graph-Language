4ac922764066ef512e629fa7e42d2465cc872b1b
Theodor Risager <theodor349@gmail.com>
2021-03-18 10:47:42 +0100

    Update ReferenceHandlerTests.cs

--- Compiler/Tests/ReferenceHandlerLib.Tests/ReferenceHandlerTests.cs
+++ Compiler/Tests/ReferenceHandlerLib.Tests/ReferenceHandlerTests.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using NSubstitute;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ASTLib.Nodes.ExpressionNodes;
 
 namespace ReferenceHandlerLib.Tests
 {
@@ -9,9 +12,36 @@ namespace ReferenceHandlerLib.Tests
     public class ReferenceHandlerTests
     {
         [TestMethod]
-        public void Test()
-        {
+        //          FuncName_GivenInput                _CheckingFor
+        public void Dispatch_IdentifierExpAndStringList_CorrectListPassedToVisitIdentifier()
+        {   
+            List<string> expected = new List<string>() { "id" };
+            IdentifierExpression input1 = new IdentifierExpression("id", 1, 1);
+            List<string> input2 = new List<string>() { "id" };
+            IReferenceHelper helper = Substitute.For<IReferenceHelper>();
+            ReferenceHandler refHandler = new ReferenceHandler(helper);
 
+            List<string> res = new List<string>();
+            refHandler.Dispatch(input1, input2);
+
+            helper.VisitIdentifier(Arg.Any<IdentifierExpression>(), Arg.Do<List<string>>(x => res = x));
+            res.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void Dispatch_IdentifierExpAndStringList_CorrectIdentifierExpPassedToVisitIdentifier()
+        {
+            IdentifierExpression expected = new IdentifierExpression("id", 1, 1);
+            IdentifierExpression input1 = expected;
+            List<string> input2 = new List<string>() { "id" };
+            IReferenceHelper helper = Substitute.For<IReferenceHelper>();
+            ReferenceHandler refHandler = new ReferenceHandler(helper);
+
+            IdentifierExpression res = null;
+            refHandler.Dispatch(input1, input2);
+
+            helper.VisitIdentifier(Arg.Do<IdentifierExpression>(x => res = x), Arg.Any<List<string>>());
+            res.Should().BeEquivalentTo(expected);
         }
     }
 }
