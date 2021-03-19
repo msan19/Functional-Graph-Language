c10c5a066fe8693a72257b9af04203599cee9f0f
Alan Khorsid <alan18@hotmail.dk>
2021-03-19 13:34:31 +0100

    FunctionCallInteger Implementation

--- Compiler/src/InterpreterLib/Interfaces/IInterpreter.cs
+++ Compiler/src/InterpreterLib/Interfaces/IInterpreter.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ASTLib;
+using ASTLib.Nodes;
 using ASTLib.Nodes.ExpressionNodes;
 
 namespace InterpreterLib.Interfaces
@@ -7,8 +9,17 @@ namespace InterpreterLib.Interfaces
     public interface IInterpreter
     {
         int DispatchFunction(ExpressionNode node, List<object> parameters);
+
         int DispatchInt(ExpressionNode node, List<object> parameters);
+
         double DispatchReal(ExpressionNode node, List<object> parameters);
+
         List<double> Interpret(AST node);
+
+        int FunctionInteger(FunctionNode node, List<Object> parameters);
+
+        double FunctionReal(FunctionNode node, List<Object> parameters);
+
+        int FunctionFunction(FunctionNode node, List<Object> parameters);
     }
 }
