4d63e9655484141f686315e68d969f4865d3bd85
Simonbolsen <Simonbolsen1234@gmail.com>
2021-03-19 11:29:33 +0100

    more interpreter tests

--- Compiler/src/InterpreterLib/Interpreter.cs
+++ Compiler/src/InterpreterLib/Interpreter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using ASTLib;
+using ASTLib.Nodes;
 using ASTLib.Nodes.ExpressionNodes;
+using ASTLib.Nodes.ExpressionNodes.OperationNodes;
 using InterpreterLib.Helpers;
 using InterpreterLib.Interfaces;
 
@@ -27,12 +29,22 @@ namespace InterpreterLib
 
         public List<double> Interpret(AST node)
         {
-            throw new NotImplementedException();
+            List<double> results = new List<double>();
+            foreach (ExportNode n in node.Exports) results.Add(_realHelper.ExportReal(n, new List<object>()));
+            return results;
         }
 
         public int DispatchInt(ExpressionNode node, List<Object> parameters)
         {
-            throw new NotImplementedException();
+            return node switch
+            {
+                SubtractionExpression e     => _integerHelper.SubtractionInteger(e, parameters),
+                AdditionExpression e        => _integerHelper.AdditionInteger(e, parameters),
+                MultiplicationExpression e  => _integerHelper.MultiplicationInteger(e, parameters),
+                DivisionExpression e        => _integerHelper.DivisionInteger(e, parameters),
+                FunctionCallExpression e    => _integerHelper.FunctionCallInteger(e, parameters),
+                _ => throw new Exception($"{node.GetType()} has not been implemented in DispatchFunction")
+            };
         }
 
         public double DispatchReal(ExpressionNode node, List<Object> parameters)
@@ -44,6 +56,7 @@ namespace InterpreterLib
         {
             return node switch
             {
+                IdentifierExpression e => _functionHelper.IdentifierFunction(e, parameters),
                 FunctionCallExpression e => _functionHelper.FunctionCallFunction(e, parameters),
                 _ => throw new Exception($"{node.GetType()} has not been implemented in DispatchFunction")
             };
