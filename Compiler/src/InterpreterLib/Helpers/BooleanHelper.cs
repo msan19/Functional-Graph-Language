5099b061044244949b8a03a15edcedabb1b076fd
Alan Khorsid <alan18@hotmail.dk>
2021-03-29 11:24:08 +0200

    BooleanHelper and IBooleanHelpersetup

---
+++ Compiler/src/InterpreterLib/Helpers/BooleanHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using ASTLib;
+using ASTLib.Nodes;
+using ASTLib.Nodes.ExpressionNodes;
+using ASTLib.Nodes.ExpressionNodes.BooleanOperationNodes;
+using ASTLib.Nodes.ExpressionNodes.CommonOperationNodes;
+using ASTLib.Nodes.ExpressionNodes.CommonOperationNodes.RelationalOperationNodes;
+using ASTLib.Nodes.TypeNodes;
+using InterpreterLib.Interfaces;
+
+namespace InterpreterLib.Helpers
+{
+    public class BooleanHelper : IBooleanHelper
+    {
+        public IInterpreter Interpreter { get; set; }
+        private Func<ExpressionNode, List<object>, bool> _dispatchBoolean;
+        private Func<ExpressionNode, List<object>, TypeEnum, object> _dispatch;
+        private Func<FunctionNode, List<object>, bool> _functionBoolean;
+        private AST _root;
+
+        public bool AndBoolean(AndExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ConditionBoolean(ConditionNode node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EqualBoolean(EqualExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FunctionCallBoolean(FunctionCallExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GreaterBoolean(GreaterExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GreaterEqualBoolean(GreaterEqualExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IdentifierBoolean(IdentifierExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LessBoolean(LessExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LessEqualBoolean(LessEqualExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NotBoolean(NotExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NotEqualBoolean(NotEqualExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OrBoolean(OrExpression node, List<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetASTRoot(AST root)
+        {
+            _root = root;
+        }
+
+        public void SetUpFuncs(Func<ExpressionNode, List<object>, bool> dispatchBoolean,
+                               Func<ExpressionNode, List<object>, TypeEnum, object> dispatch,
+                               Func<FunctionNode, List<object>, bool> functionBoolean)
+        {
+            _dispatchBoolean = dispatchBoolean;
+            _dispatch = dispatch;
+            _functionBoolean = functionBoolean;
+        }
+    }
+}
---
+++ Compiler/src/InterpreterLib/Interfaces/IBooleanHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using ASTLib;
+using ASTLib.Nodes;
+using ASTLib.Nodes.ExpressionNodes;
+using ASTLib.Nodes.ExpressionNodes.BooleanOperationNodes;
+using ASTLib.Nodes.ExpressionNodes.CommonOperationNodes;
+using ASTLib.Nodes.ExpressionNodes.CommonOperationNodes.RelationalOperationNodes;
+using ASTLib.Nodes.TypeNodes;
+
+namespace InterpreterLib.Interfaces
+{
+    public interface IBooleanHelper
+    {
+        IInterpreter Interpreter { get; set; }
+
+        void SetASTRoot(AST root);
+
+        void SetUpFuncs(Func<ExpressionNode, List<Object>, bool> dispatchBoolean,
+               Func<ExpressionNode, List<object>, TypeEnum, Object> dispatch,
+               Func<FunctionNode, List<object>, bool> functionBoolean);
+
+        bool ConditionBoolean(ConditionNode node, List<Object> parameters);
+
+        bool IdentifierBoolean(IdentifierExpression node, List<Object> parameters);
+
+        bool NotBoolean(NotExpression node, List<object> parameters);
+
+        bool AndBoolean(AndExpression node, List<object> parameters);
+
+        bool OrBoolean(OrExpression node, List<object> parameters);
+
+        bool EqualBoolean(EqualExpression node, List<object> parameters);
+
+        bool NotEqualBoolean(NotEqualExpression node, List<object> parameters);
+
+        bool GreaterEqualBoolean(GreaterEqualExpression node, List<object> parameters);
+
+        bool GreaterBoolean(GreaterExpression node, List<object> parameters);
+
+        bool LessEqualBoolean(LessEqualExpression node, List<object> parameters);
+
+        bool LessBoolean(LessExpression node, List<object> parameters);
+
+        bool FunctionCallBoolean(FunctionCallExpression node, List<object> parameters);
+    }
+}
