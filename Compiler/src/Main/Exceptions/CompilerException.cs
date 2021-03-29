e424ce4f63a6b3f3319b8bab39c0393b822a67ae
Simonbolsen <Simonbolsen1234@gmail.com>
2021-03-29 12:13:25 +0200

    CompilerException

---
+++ Compiler/src/Main/Exceptions/CompilerException.cs
@@ -0,0 +1,18 @@
+﻿using ASTLib.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    public class CompilerException: Exception
+    {
+        private Node _node;
+
+        public CompilerException(Node node)
+        {
+            _node = node;
+        }
+
+    }
+}
