c1e2cecd30c21f475b757ef5d1d21f7681f6ac42
Theodor Risager <theodor349@gmail.com>
2021-03-29 11:12:30 +0200

    Create ITypeHelper.cs

---
+++ Compiler/src/TypeCheckerLib/Interfaces/ITypeHelper.cs
@@ -0,0 +1,14 @@
+﻿using ASTLib;
+using ASTLib.Nodes.ExpressionNodes;
+using ASTLib.Nodes.TypeNodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeCheckerLib.Interfaces
+{
+    public interface ITypeHelper
+    {
+        void Initialize(AST root, Func<ExpressionNode, List<TypeNode>, TypeNode> dispatcher);
+    }
+}
