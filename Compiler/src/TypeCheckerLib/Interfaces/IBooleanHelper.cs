a4454ecbce48c451c15913fbade4eaa1205834f4
Philip Christian Greve <pgreve19@student.aau.dk>
2021-04-07 13:01:38 +0200

    Visit boolean operation functions

--- Compiler/src/TypeCheckerLib/Interfaces/IBooleanHelper.cs
+++ Compiler/src/TypeCheckerLib/Interfaces/IBooleanHelper.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using ASTLib;
 using ASTLib.Nodes.ExpressionNodes;
+using ASTLib.Nodes.ExpressionNodes.BooleanOperationNodes;
 using ASTLib.Nodes.TypeNodes;
 
 namespace TypeCheckerLib.Interfaces
 {
     public interface IBooleanHelper : ITypeHelper
     {
+        TypeNode VisitAnd(AndExpression node, List<TypeNode> parameterTypes);
+        TypeNode VisitNot(NotExpression node, List<TypeNode> parameterTypes);
+        TypeNode VisitOr(OrExpression node, List<TypeNode> parameterTypes);
     }
 }
