c79028343dbe55b4de6a8b6760245ea119deef16
Alan Khorsid <alan18@hotmail.dk>
2021-04-08 10:08:32 +0200

    more tests

--- Compiler/src/ASTLib/Nodes/ExpressionNodes/CommonOperationNodes/RelationalOperationNodes/GreaterExpression.cs
+++ Compiler/src/ASTLib/Nodes/ExpressionNodes/CommonOperationNodes/RelationalOperationNodes/GreaterExpression.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using ASTLib.Interfaces;
+using ASTLib.Nodes.TypeNodes;
 
 namespace ASTLib.Nodes.ExpressionNodes.CommonOperationNodes.RelationalOperationNodes
 {
@@ -7,5 +8,7 @@ namespace ASTLib.Nodes.ExpressionNodes.CommonOperationNodes.RelationalOperationN
     {
         public GreaterExpression(ExpressionNode leftExpression, ExpressionNode rightExpression, int line, int letter) 
             : base(new List<ExpressionNode> { leftExpression, rightExpression }, line, letter) {}
+
+        public TypeEnum Type { get; set; }
     }
 }
