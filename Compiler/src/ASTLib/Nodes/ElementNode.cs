deb853d9cc87fee92e827dd953fa0b15327aed19
Simonbolsen <Simonbolsen1234@gmail.com>
2021-04-12 14:02:43 +0200

    Small changes

--- Compiler/src/ASTLib/Nodes/ElementNode.cs
+++ Compiler/src/ASTLib/Nodes/ElementNode.cs
@@ -9,11 +9,13 @@ namespace ASTLib.Nodes
     {
         public string ElementIdentifier { get; }
         public List<string> IndexIdentifiers { get; }
+        public int Reference { get; set; }
 
         public ElementNode(string elementName, List<string> identifiers, int line, int letter) : base(line, letter)
         {
             ElementIdentifier = elementName;
             IndexIdentifiers = identifiers;
+            Reference = -1;
         }
     }
 }
