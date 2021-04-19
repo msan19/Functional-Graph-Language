6b4e7baa7f03fa5b64b262965eb9bb5d24b37b18
Philip Christian Greve <pgreve19@student.aau.dk>
2021-04-19 11:28:16 +0200

    New CastFrom classes (added in new folder)

---
+++ Compiler/src/ASTLib/Nodes/ExpressionNodes/CastExpressionNodes/CastFromBooleanExpression.cs
@@ -0,0 +1,12 @@
+﻿using ASTLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASTLib.Nodes.ExpressionNodes.CastExpressionNodes
+{
+    public class CastFromBooleanExpression : ExpressionNode, INonIdentifierExpression
+    {
+        public CastFromBooleanExpression(ExpressionNode child, int line, int letter) : base(new List<ExpressionNode> { child }, line, letter) { }
+    }
+}
