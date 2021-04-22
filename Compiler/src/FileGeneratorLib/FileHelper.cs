1989c8558c6850d1a3b56f3a409abe7acd4fa487
msan19 <msan19@student.aau.dk>
2021-04-22 12:41:47 +0200

    FileGenerator now adds labels

--- Compiler/src/FileGeneratorLib/FileHelper.cs
+++ Compiler/src/FileGeneratorLib/FileHelper.cs
@@ -1,7 +1,44 @@
+using System;
+using System.IO;
+
 namespace FileGeneratorLib
 {
     public class FileHelper
     {
+        private const string UNIX_PREFIX = "Unix";
+        private const string WINDOWS_PREFIX = "Microsoft Windows";
         
+        public string GetPathWith(string fileName)
+        {
+            string path = "";
+            Console.WriteLine($"OS: {Environment.OSVersion}");
+            
+            string projectDirectory = GetProjectDirectory();
+            Console.WriteLine(projectDirectory);
+
+            if (IsUnix)
+                path = $"{projectDirectory}/{fileName}";
+            else if (IsWindows)
+                path = $"{projectDirectory}\\{fileName}";
+
+            return path;
+        }
+        
+        public string GetProjectDirectory()
+        {
+            string separator = null;
+            string projectDirectory = Directory.GetCurrentDirectory();
+            if (IsUnix)
+                separator = "/";
+            else if (IsWindows)
+                separator = "\\";
+            string[] dirNames = projectDirectory.Split(separator);
+            if (dirNames.Length >= 3 && dirNames[dirNames.Length - 3].Equals("bin"))
+                projectDirectory = (Directory.GetParent(projectDirectory).Parent).Parent.FullName;
+            return projectDirectory;
+        }
+        
+        private bool IsUnix => Environment.OSVersion.ToString().StartsWith(UNIX_PREFIX);
+        private bool IsWindows => Environment.OSVersion.ToString().StartsWith(WINDOWS_PREFIX);
     }
 }
