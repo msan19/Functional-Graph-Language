cfa686281bfae57596cec95100eda5577173bea5
Theodor Risager <theodor349@gmail.com>
2021-04-23 08:42:09 +0200

    Removed outputFolder from Git

--- Compiler/src/Main/Program.cs
+++ Compiler/src/Main/Program.cs
@@ -128,7 +128,7 @@ namespace Main
 
         private void ParseArgs(string[] args)
         {
-            _fileNames = new List<string>() /*{ "bTree.fgl" }*/;
+            _fileNames = new List<string>() { "bTree.fgl" };
             _saveOutput = true;
             foreach (string s in args)
             {
