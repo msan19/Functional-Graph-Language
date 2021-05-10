94a02da7f186aa1b9a3c83ed003eecd22cabbda1
Theodor Risager <theodor349@gmail.com>
2021-05-10 09:34:33 +0200

    Integration TypeChecker TestCases outline

---
+++ Compiler/Tests/IntegrationTesting/Single/SingleCompTypeCheckerLib.Tests/TypeCheckerTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SingleCompTypeCheckerLib.Tests
+{
+    public static class Utilities
+    {
+
+    }
+
+    [TestClass]
+    public class TypeCheckerTests
+    {
+        // Insert Cast Node
+        // Throw error on invalid Types
+        // Anonymous Function
+        //  Chakc if it is correct in given context
+        //  Move Anonym to global scope 
+        // Removed irrelevant references on functions
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
