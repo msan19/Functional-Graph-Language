a8b1d5c6be9ae09d0a32e7c3a1b126355fe8acb5
Theodor Risager <theodor349@gmail.com>
2021-05-10 13:13:23 +0200

    Integration TypeChecker Irrelevant Function References

--- Compiler/Tests/IntegrationTesting/Single/SingleCompTypeCheckerLib.Tests/TypeCheckerTests.cs
+++ Compiler/Tests/IntegrationTesting/Single/SingleCompTypeCheckerLib.Tests/TypeCheckerTests.cs
@@ -8,6 +8,7 @@ using ASTLib.Nodes.TypeNodes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TypeCheckerLib;
 using TypeCheckerLib.Helpers;
 using TypeBooleanHelper = TypeCheckerLib.Helpers.BooleanHelper;
@@ -42,9 +43,18 @@ namespace SingleCompTypeCheckerLib.Tests
             return res;
         }
 
-        internal static FunctionNode GetFunction(string id, TypeEnum output, List<TypeEnum> inputTypes, List<string> ids, ConditionNode condition)
+        internal static FunctionNode GetFunction(TypeEnum output, List<TypeEnum> inputTypes, ConditionNode condition)
         {
-            return new FunctionNode(id, condition, ids, GetFunctionTypeNode(inputTypes, output), 0, 0);
+            return new FunctionNode("Func", condition, new List<string>(), GetFunctionTypeNode(inputTypes, output), 0, 0);
+        }
+        internal static FunctionNode GetFunction(TypeEnum output, List<TypeNode> inputTypes, ConditionNode condition)
+        {
+            return new FunctionNode("Func", condition, new List<string>(), GetFunctionTypeNode(inputTypes, output), 0, 0);
+        }
+
+        private static FunctionTypeNode GetFunctionTypeNode(List<TypeNode> inputTypes, TypeEnum output)
+        {
+            return new FunctionTypeNode(GetTypeNode(output), inputTypes, 0, 0);
         }
 
         private static FunctionTypeNode GetFunctionTypeNode(List<TypeEnum> inputTypes, TypeEnum output)
@@ -78,17 +88,196 @@ namespace SingleCompTypeCheckerLib.Tests
                                            new TypeBooleanHelper(),
                                            new TypeSetHelper());
         }
+
+        internal static List<int> GetIntList(int num)
+        {
+            var res = new List<int>();
+            for (int i = 0; i < num; i++)
+                res.Add(i);
+            return res;
+        }
+        internal static List<int> GetIntList(int start, int end)
+        {
+            var res = new List<int>();
+            for (int i = start; i < end; i++)
+                res.Add(i);
+            return res;
+        }
+
+        internal static FunctionCallExpression GetFunctionCallExpr(List<int> globals, int local, List<ExpressionNode> inputs)
+        {
+            var res = new FunctionCallExpression("func", inputs, 0, 0);
+            res.GlobalReferences = globals;
+            res.LocalReference = local;
+            return res;
+        }
+
+        internal static List<ExpressionNode> GetIntLitList(int num)
+        {
+            var res = new List<ExpressionNode>();
+            for (int i = 0; i < num; i++)
+                res.Add(new IntegerLiteralExpression(i, 0, 0));
+            return res;
+        }
+
+        internal static void AddFunctions(AST ast, int num, TypeEnum output, TypeEnum input, TypeEnum wrongType)
+        {
+            var inputs = new List<TypeEnum>();
+            for (int i = 0; i < num; i++)
+                inputs.Add(wrongType);
+            for (int i = 0; i < num; i++)
+            {
+                inputs[i] = (input);
+                AddFunctionNode(ast, GetFunction(output, inputs, GetCondition(GetIntLit())));
+            }
+        }
+
+        internal static ExpressionNode GetIntLit()
+        {
+            return new IntegerLiteralExpression(0, 0, 0);
+        }
+
+        internal static ExpressionNode GetBoolLit()
+        {
+            return new BooleanLiteralExpression(false, 0, 0);
+        }
+
+        internal static ExpressionNode GetLit(TypeEnum x)
+        {
+            return x switch
+            {
+                TypeEnum.InvalidType => throw new NotImplementedException(),
+                TypeEnum.Real => GetRealLit(),
+                TypeEnum.Integer => GetIntLit(),
+                TypeEnum.Boolean => GetBoolLit(),
+                TypeEnum.String => GetStringLit(),
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        internal static StringLiteralExpression GetStringLit()
+        {
+            return new StringLiteralExpression("string", 0, 0);
+        }
+
+        internal static RealLiteralExpression GetRealLit()
+        {
+            return new RealLiteralExpression(0, 0, 0);
+        }
+
+        internal static TypeNode GetFunctionType(TypeEnum returnType, List<TypeEnum> inputs)
+        {
+            return new FunctionTypeNode(GetTypeNode(returnType), GetTypeNode(inputs), 0, 0);
+        }
     }
 
     [TestClass]
     public class TypeCheckerTests
     {
+        private static int NO_LOCAL_REF => FunctionCallExpression.NO_LOCAL_REF;
         // Insert Cast Node
-        // Throw error on invalid Types
-        // Anonymous Function
-        //  Chakc if it is correct in given context
-        //  Move Anonym to global scope 
         // Removed irrelevant references on functions
+        //  Global
+        //  Local
+        //  Both
+        // Anonymous Function
+        //  Check if it is correct in given context
+        //  Move Anonym to global scope 
+        // Throw error on invalid Types
+
+        #region Irrelevant Function References 
+        [DataRow(new TypeEnum[] { TypeEnum.Integer }, new TypeEnum[] { TypeEnum.Boolean })]
+        [TestMethod]
+        public void RemoveIrrelevantFuncReferences_OneGlobalOneReference_LocalScope(TypeEnum[] global, TypeEnum[] local)
+        {
+            var ast = Utilities.GetAstSkeleton();
+
+            // Global
+            var expr = Utilities.GetIntLit();
+            var cond = Utilities.GetCondition(expr);
+            var func = Utilities.GetFunction(TypeEnum.Integer, global.ToList(), cond);
+            Utilities.AddFunctionNode(ast, func);
+            // Local
+            var funcType = Utilities.GetFunctionType(TypeEnum.Integer, local.ToList());
+            // Function Call
+            int globalRef = 0;
+            int localRef = 0;
+            var inputs = local.ToList().ConvertAll(x => Utilities.GetLit(x));
+            expr = Utilities.GetFunctionCallExpr(new List<int>() { globalRef }, localRef, inputs);
+            cond = Utilities.GetCondition(expr);
+            func = Utilities.GetFunction(TypeEnum.Integer, new List<TypeNode>() { funcType }, cond);
+            Utilities.AddFunctionNode(ast, func);
+
+            var typeChecker = Utilities.GetTypeChecker();
+            typeChecker.CheckTypes(ast);
+            var res = (FunctionCallExpression)expr;
+
+            Assert.AreEqual(localRef, res.LocalReference);
+            Assert.AreEqual(0, res.GlobalReferences.Count);
+        }
+        [DataRow(new TypeEnum[] { TypeEnum.Integer }, new TypeEnum[] { TypeEnum.Boolean })]
+        [TestMethod]
+        public void RemoveIrrelevantFuncReferences_OneGlobalOneReference_GlobalScope(TypeEnum[] global, TypeEnum[] local)
+        {
+            var ast = Utilities.GetAstSkeleton();
+
+            // Global
+            var expr = Utilities.GetIntLit();
+            var cond = Utilities.GetCondition(expr);
+            var func = Utilities.GetFunction(TypeEnum.Integer, global.ToList(), cond);
+            Utilities.AddFunctionNode(ast, func);
+            // Local
+            var funcType = Utilities.GetFunctionType(TypeEnum.Integer, local.ToList());
+            // Function Call
+            int globalRef = 0;
+            int localRef = 0;
+            var inputs = global.ToList().ConvertAll(x => Utilities.GetLit(x));
+            expr = Utilities.GetFunctionCallExpr(new List<int>() { globalRef }, localRef, inputs);
+            cond = Utilities.GetCondition(expr);
+            func = Utilities.GetFunction(TypeEnum.Integer, new List<TypeNode>() { funcType }, cond);
+            Utilities.AddFunctionNode(ast, func);
+
+            var typeChecker = Utilities.GetTypeChecker();
+            typeChecker.CheckTypes(ast);
+            var res = (FunctionCallExpression)expr;
+
+            Assert.AreEqual(NO_LOCAL_REF, res.LocalReference);
+            Assert.AreEqual(1, res.GlobalReferences.Count);
+            Assert.AreEqual(globalRef, res.GlobalReferences[0]);
+        }
+        [DataRow(3,1)]
+        [DataRow(3,2)]
+        [DataRow(3,3)]
+        [TestMethod]
+        public void RemoveIrrelevantFuncReferences_xGlobalReferences_OneReference(int numFuncs, int correctNum)
+        {
+            int expected = correctNum - 1;
+            var output = TypeEnum.Integer;
+            var input = TypeEnum.Integer;
+            var wrongType = TypeEnum.Boolean;
+            var typeParams = new List<TypeEnum>();
+
+            var globals = Utilities.GetIntList(numFuncs);
+            var local = NO_LOCAL_REF;
+            var inputs = Utilities.GetIntLitList(correctNum);
+            for (int i = correctNum; i < numFuncs; i++)
+                inputs.Add(Utilities.GetBoolLit());
+
+            var ast = Utilities.GetAstSkeleton();
+            Utilities.AddFunctions(ast, numFuncs, output, input, wrongType);
+
+            var expr = Utilities.GetFunctionCallExpr(globals, local, inputs);
+            var cond = Utilities.GetCondition(expr);
+            var func = Utilities.GetFunction(output, typeParams, cond);
+            Utilities.AddFunctionNode(ast, func);
+
+            var typeChecker = Utilities.GetTypeChecker();
+            typeChecker.CheckTypes(ast);
+
+            Assert.AreEqual(1, expr.GlobalReferences.Count);
+            Assert.AreEqual(expected, expr.GlobalReferences[0]);
+        }
+        #endregion
 
         #region Cast Node
         [DataRow(TypeEnum.Real, TypeEnum.Integer, TypeEnum.Real)]
@@ -108,7 +297,7 @@ namespace SingleCompTypeCheckerLib.Tests
             var ast = Utilities.GetAstSkeleton();
             var expr = Utilities.GetAdditionExpr(leftId, leftRef, rightId, rightRef);
             var cond = Utilities.GetCondition(expr);
-            var func = Utilities.GetFunction("func", output, types, ids, cond);
+            var func = Utilities.GetFunction(output, types, cond);
             Utilities.AddFunctionNode(ast, func);
 
             var typeChecker = Utilities.GetTypeChecker();
@@ -120,7 +309,6 @@ namespace SingleCompTypeCheckerLib.Tests
                 res == typeof(CastFromBooleanExpression) ||
                 res == typeof(CastFromIntegerExpression));
         }
-
         #endregion
     }
 }
